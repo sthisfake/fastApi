55620f6a3087b92d34db70bda92a4cf4ba6d67a2
pouya teimoury <pouya.gachboy@gmail.com>
2023-05-02 14:31:07 +0430

    postgres with chosen schema fastapi using sqlalchemy

---
+++ simple_connection_user_item_postgres/database.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:[password]@localhost:5432/postgres"
+dbschema = 'test'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+
+    # defining the schema name
+    connect_args={'options': '-csearch_path={}'.format(dbschema)}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
